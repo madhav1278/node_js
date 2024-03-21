a378a5bcc2051a92df364db726397ae93f7a5b81
Your Name <you@example.com>
2024-03-21 07:54:38 +0530

    mydb

--- mydb.js
+++ mydb.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 // const mongoURL=process.env.MONGODB_URL_LOCAL;
 const mongoURL=process.env.MONGODB_URL;
 
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+// mongoose.connect(mongoURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
 
 mongoose.connect(mongoURL)
 
